654ab223a382e20bd36c3f6520f6e51da9584f52
yazidbky <yazid.bekkai@univ-msila.dz>
2025-03-09 10:55:10 +0100

    4th commit

--- src/index.mjs
+++ src/index.mjs
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use('/api/auth', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB connected'))
