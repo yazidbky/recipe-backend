522df0b64df5a269aecf2d3f2b7dd9dca63fa76f
Yazid BK <yazid.bekkai@univ-msila.dz>
2025-03-19 01:09:08 +0100

    GG

--- src/index.mjs
+++ src/index.mjs
@@ -29,10 +29,7 @@ if (!process.env.MONGO_URI) {
 }
 
 // Connect to MongoDB & Start Server
-mongoose.connect(process.env.MONGO_URI , {
-     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI ,)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
