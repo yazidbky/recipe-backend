e762dd38291dcb5aba9fb5f5e6c18803e8a9494a
Yazid BK <yazid.bekkai@univ-msila.dz>
2025-03-19 01:00:20 +0100

    commit

--- src/index.mjs
+++ src/index.mjs
@@ -29,7 +29,10 @@ if (!process.env.MONGO_URI) {
 }
 
 // Connect to MongoDB & Start Server
-mongoose.connect(process.env.MONGO_URI ,)
+mongoose.connect(process.env.MONGO_URI , {
+     useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
