9a718bf0f2a07fdd7cc54d6ec597ccb503c7e261
Yazid BK <yazid.bekkai@univ-msila.dz>
2025-03-25 06:03:47 +0100

    changed a mistake

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/:id', getUser);
+router.get('/:id', getUser);
 
 export default router;
